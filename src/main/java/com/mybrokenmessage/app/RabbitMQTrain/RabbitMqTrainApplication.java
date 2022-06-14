847f561402d1ca6a6e392486f9f9549d616119c3
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-14 18:13:13 -0300

    Initial Project

---
+++ src/main/java/com/mybrokenmessage/app/RabbitMQTrain/RabbitMqTrainApplication.java
@@ -0,0 +1,13 @@
+package com.mybrokenmessage.app.RabbitMQTrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybrokenmessage/app/RabbitMQTrain/RabbitMqTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybrokenmessage.app.RabbitMQTrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
